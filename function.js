a3dd9dc360956f0069eb94e527bd0b89293c9c1c
ArellanoF <CORREO@example.comfrancescarellano@gmail.com>
2021-11-28 13:52:02 +0100

    portfolio

--- function.js
+++ function.js
@@ -48,3 +48,24 @@ if(document.querySelector('#container-slider')){
      elements[nextElement].style.opacity=1;
      elements[nextElement].style.zIndex =1;
  }
+
+
+    function fireSweetAlert() {
+        Swal.fire({
+        icon: 'info',
+        text: 'francescarellano@gmail.com',
+        padding: 10,
+        animation: true,
+        imageUrl: './assets/icons/mail.jpg'
+    })
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
